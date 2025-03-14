f4754ab22bcdf02b4a0b80283592d8503f1340d2
leticia1504 <144252721+leticia1504@users.noreply.github.com>
2025-03-14 12:16:58 -0300

    salvar

--- script.js
+++ script.js
@@ -28,13 +28,13 @@ document.addEventListener('DOMContentLoaded', function(){
          tamanhoAtualFonte -= 0.1;
          document.body.style.fontSize = `${tamanhoAtualFonte}rem`
  
-     })
+        })
  
-     alternaContraste.addEventListener('click', function(){
-         document.body.classList.toggle('alto-contraste')
-     })
- 
- 
+        alternaContraste.addEventListener('click', function(){
+            document.body.classList.toggle('alto-contraste')
+        })
+    
+    
  })
  
  ScrollReveal().reveal('#inicio', { delay: 500 });
